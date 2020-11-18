40289ca5bb95930c317d912a8b3f650cb886ce75
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-18 16:03:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,16 +1,9 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  return array.index(value_to_find)
+  #first option to solve this
+  #return array.index(value_to_find)
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  #using while
+  arr_len = array.size
+  count = 1
 end
